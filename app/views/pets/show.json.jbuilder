e1b1814a4a935aef3b8ecba6cf441475989a9d43
Marta Walczak <mwalczak@sigma.ug.edu.pl>
2016-04-08 15:56:13 +0200

    added the model for pets table

---
+++ app/views/pets/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @pet, :id, :name, :species, :description, :created_at, :updated_at
---
+++ db/migrate/20160408134657_create_pets.rb
@@ -0,0 +1,11 @@
+class CreatePets < ActiveRecord::Migration[5.0]
+  def change
+    create_table :pets do |t|
+      t.string :name
+      t.string :species
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
