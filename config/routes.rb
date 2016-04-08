1b7cb82126db1b0c695234ec614c04cba3a97eb4
Marta Walczak <mwalczak@sigma.ug.edu.pl>
2016-04-08 23:37:11 +0200

    added homepage and changed index page of pets

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
   resources :pets
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
 
-  root 'pets#index'
+  get '/:id', to: 'pets#show', constraints: { id: /\d+/ }
+  
+  root 'static_pages#home' 
 end
