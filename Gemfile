2546a705fd8bb29678d21c4d22d5a39106a4704a
Marta Walczak <mwalczak@sigma.ug.edu.pl>
2016-04-09 18:32:26 +0200

    added pagination

--- Gemfile
+++ Gemfile
@@ -34,6 +34,8 @@ gem 'jbuilder', '~> 2.0'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'will_paginate', '~> 3.1.0'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,28 +1,20 @@
 class PetsController < ApplicationController
   before_action :set_pet, only: [:show, :edit, :update, :destroy]
 
-  # GET /pets
-  # GET /pets.json
   def index
-    @pets = Pet.all
+    @pets = Pet.all.order('updated_at DESC').page(params[:page]).per_page(3)
   end
 
-  # GET /pets/1
-  # GET /pets/1.json
   def show
   end
 
-  # GET /pets/new
   def new
     @pet = Pet.new
   end
 
-  # GET /pets/1/edit
   def edit
   end
 
-  # POST /pets
-  # POST /pets.json
   def create
     @pet = Pet.new(pet_params)
 
@@ -37,8 +29,6 @@ class PetsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /pets/1
-  # PATCH/PUT /pets/1.json
   def update
     respond_to do |format|
       if @pet.update(pet_params)
@@ -51,8 +41,6 @@ class PetsController < ApplicationController
     end
   end
 
-  # DELETE /pets/1
-  # DELETE /pets/1.json
   def destroy
     @pet.destroy
     respond_to do |format|
