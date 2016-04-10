f8e8a101d96e46e1865f66b52df1d4f50d13dc47
Marta Walczak <mwalczak@sigma.ug.edu.pl>
2016-04-10 16:31:12 +0200

    added login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'static_pages/home'
   resources :pets
   resources :users
 
   root 'static_pages#home'
   get 'signup' => 'users#new'
-  get '/:id', to: 'pets#show', constraints: { id: /\d+/ }
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 end
