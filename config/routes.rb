73ca4ca9cfb85eba9517061c67d03a33854a415b
Marta Walczak <mwalczak@sigma.ug.edu.pl>
2016-04-10 13:48:57 +0200

    added controller users, signup page and footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
-
   resources :pets
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  # Serve websocket cable requests in-process
-  # mount ActionCable.server => '/cable'
-
+  root 'static_pages#home'
+  get 'signup' => 'users#new'
   get '/:id', to: 'pets#show', constraints: { id: /\d+/ }
-  
-  root 'static_pages#home' 
 end
