5e1cdf58e29d2ed2b4eca98cb491454bdc80ffc5
Marta Walczak <mwalczak@sigma.ug.edu.pl>
2016-04-10 20:10:22 +0200

    changed seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-Pet.create(name:'Kulka',species:'Kot',desc:'Czarna kotka z zielonymi oczami')
-Pet.create(name:'Reks',species:'Pies',desc:'Szary wilczur, niebieskie oczy')
+Pet.create!(name:'Alex',species:'Cat',desc:'Black cat with green eyes')
+Pet.create!(name:'Reks',species:'Dog',desc:'Grey wolf, brown eyes')
+Pet.create!(name:'Luna',species:'Fish',desc:'Gold fish')
+10.times do
+  Pet.create(
+    name: (Faker::Name.name).split(' ').first,
+    species: 'Dog',
+    desc: "Little white doggy with #{Faker::Comerce.color} eyes"
+  )
+end
 
-User.create(name:'Marta',email:'admin@example.com',password:'passadmin',admin:true)
-
+User.create!(name:'Admin',email:'admin@example.com',password:'passadmin',admin:true)
+User.create!(name:'Other',email:'other@example.com',password:'passother')
