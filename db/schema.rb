bccd5d391fe9fa176ca8eba3d46359ac634e2a7d
Marta Walczak <mwalczak@sigma.ug.edu.pl>
2016-04-10 14:10:04 +0200

    added model of user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160409163748) do
+ActiveRecord::Schema.define(version: 20160410120521) do
 
   create_table "pets", force: :cascade do |t|
     t.string   "name"
@@ -21,5 +21,14 @@ ActiveRecord::Schema.define(version: 20160409163748) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
